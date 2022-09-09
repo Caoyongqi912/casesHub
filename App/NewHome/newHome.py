145e45962e1028f5e7571920869a92a97108202f
‘caoyongqi <caoyongqi345@gmail.com>
2022-09-09 16:48:52 +0800

    del

--- App/NewHome/newHome.py
+++ App/NewHome/newHome.py
@@ -6,16 +6,7 @@ from MyException import Api
 from Utils.myRequestParseUtil import MyRequestParseUtil
 
 
-class NewProject(Resource):
 
-    def post(self):
-        parse = MyRequestParseUtil()
-        parse.add(name="creator", type=str, required=True)
-        parse.add(name="projectName", type=str, required=True)
-        parse.add(name="handle", type=int, choices=[1, 2, 3], default=3, required=True)
-        from Comment.newhome import createNewProject
-        createNewProject(**parse.parse_args())
-        return MyResponse.success()
 
 
 class NewPan(Resource):
@@ -30,5 +21,4 @@ class NewPan(Resource):
 
 
 api = Api(newHomeBP)
-api.add_resource(NewProject, "/newProject")
 api.add_resource(NewPan, "/newPan")
